12fd0177cb59132713d5152d27f7f7d7077b9195
TobiasRoman <tobiasroman51@gmail.com>
2022-04-08 13:20:28 -0300

    Se corrigió el carrito

--- src/routes/carroRouter.js
+++ src/routes/carroRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const carroController = require('../controllers/carroController');
 
 /* Ruta para listar productos */
-router.get('/carro-de-compra', carroController.Carro);
+router.get('/', carroController.Carro);
 
 
 module.exports = router;
