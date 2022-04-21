ffed8fd11a5812aa7ecbd3576a396713ba02dc65
Igor Villar <Theigor44@gmail.com>
2022-04-21 20:32:07 -0300

    Se agrega boceto de validator

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
+const registerValidator = require("../validations/registerValidator")
 
 /* GET - ruta login -  reenderiza vista login */
 router.get('/login', usersController.login);
 /* GET - ruta register - Reenderiza vista registro*/
 router.get('/registro', usersController.register);
 /* POST - Crea un nuevo usuario */
-router.post('/registro', usersController.processRegister)
+router.post('/registro' ,registerValidator, usersController.processRegister)
 
 
 
---
+++ src/validations/registerValidator.js
@@ -0,0 +1,26 @@
+const { check, body } = require("express-validator");
+
+let validateRegister = [
+    check("name")
+     .notEmpty().withMessage("El nombre es requerido").bail()
+     .isLength({min:2}).withMessage("Ingrese un nombre válido"),
+    check("email")
+     .notEmpty().withMessage("El email es requerido").bail
+     .isEmail().withMessage("ingrese un email válido"),
+    check("password")
+     .notEmpty().withMessage("Ingrese una contraseña")
+     .isLength({min: 8}).withMessage("La contraseña Debe tener por lo menos 8 caracteres"),
+    check("password2").notEmpty().withMessage("Reingrese su contraseña"), 
+
+    body("password2").custom((value, {req}) => {
+        if(value !== req.body.password){
+            return false;
+        }
+        return true;
+    }).withMessage("Las contraseñas no coinciden"),
+
+    check("terms")
+     .notEmpty().withMessage("Debes aceptar los términos y condiciones")
+];
+
+module.exports = validateRegister;
