f42e1979f885d96e5aae59a0630050daa2f8fd72
Javier Garcia <javii_93_27@hotmail.com>
2022-04-28 00:31:07 -0300

    se le dio logica que al ingresar con nombre Admin te lleve a los Admin y en caso de que no, te de error

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,7 +1,9 @@
+// importar JSON de usuarios administradores y cambiarlo por el let usersAdmin
+
 let usersAdmin = ["Javier","Igor","Tobias","Noelia"];   
 
 const adminMiddleware= (req, res, next) => {
-    if(usersAdmin.includes(req.query.user)) {
+    if(usersAdmin.includes(req.query.name)) {
         next();
     } else {
         res.send("No tienes permisos para ingresar")
