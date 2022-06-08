4eb77dae13fcba4812e0b2f3d945c77cbe478e34
Javier Garcia <javii_93_27@hotmail.com>
2022-06-07 23:35:19 -0300

    login validation y correcion de session

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -25,7 +25,6 @@ module.exports = {
                     name: user.name,
                     avatar:user.avatar,
                     email: user.email,
-                    rol: user.rol
                 };
                 if(req.body.remember){
                 const TIME_IN_MILISECONDS = 60000 * 60 * 24 * 365;
@@ -82,16 +81,85 @@ module.exports = {
             res.render('register', {
                 titulo: "Register",
                 errors: errors.mapped(),
-                session: req.session
+                session: req.session,
+                old: req.body
             })
         }
     
+    },
+    profile: (req, res) => {
+        db.User.findOne({
+            where: {
+                id: req.session.user.id
+            },
+            include: [{ association: "addresses" }],
+        })
+        .then((user) => {
+            res.render("users/userProfile", {
+                session: req.session,
+                user,
+                titulo: req.session.user.name,
+                css: "userProfile.css"
+            })
+        })
+    },
+    profileUpdate: (req, res) => {
+        let errors = validationResult(req);
+
+        if(errors.isEmpty()){
+            db.User.update({
+                ...req.body
+            },{
+                where: {
+                    id: req.session.user.id
+                }
+            })
+            .then(() => 
+                res.redirect("/usuarios/perfil")
+            )
+            .catch(error => res.send(error))
+        }else{
+            db.User.findOne({
+                where: {
+                    id: req.session.user.id
+                },
+                include: [{ association: "addresses" }],
+            })
+            .then((user) => {
+                res.render("users/userProfile", {
+                    session: req.session,
+                    user,
+                    titulo: req.session.user.name,
+                    css: "userProfile.css",
+                    errors: errors.mapped()
+                })
+            })
+        }
+    },
+    addressCreate: (req, res) => {
+        db.Address.create({
+            ...req.body,
+            user_id: req.session.user.id,
+        })
+        .then(() => res.redirect("/usuarios/perfil"))
+        .catch((error) => res.send(error))
+    },
+    addressDestroy: (req, res) => {
+        db.Address.destroy({
+            where: {
+                id: req.params.id,
+            }
+        })
+        .then(() => {
+            res.redirect("/perfil")    
+        })
+        .catch((error) => res.send(error))
     },
     logout: (req, res) => {
         req.session.destroy();
 
         if(req.cookies.formarCookie){
-            res.cookie('formarCookie', "", { maxAge: -1 })
+            res.cookie('Rosé', "", { maxAge: -1 })
         }
 
         res.redirect('/')
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,6 +5,8 @@ const uploadFile = require('../middlewares/uploadAvatar')
 const registerValidator = require ('../validations/registerValidator');
 const loginValidator = require ('../validations/loginValidator');
 const userInSessionCheck = require ('../middlewares/userInSessionCheck');
+const userSessionCheck = require("../middlewares/userSessionCheck");
+const profileValidator = require("../validations/profileValidator");
 
 /* GET - ruta login -  reenderiza vista login */
 router.get('/login', userInSessionCheck,usersController.login);
@@ -13,7 +15,15 @@ router.post('/login', loginValidator , usersController.processLogin);
 /* GET - ruta register - Reenderiza vista registro*/
 router.get('/registro', userInSessionCheck, usersController.register);
 /* POST - Crea un nuevo usuario y se le aplica el register validator en medio*/
-router.post('/registro', uploadFile.single('avatar') ,registerValidator ,usersController.processRegister)
+router.post('/registro', uploadFile.single('avatar') ,registerValidator ,usersController.processRegister);
+/* GET - Perfil de usuario */
+router.get('/perfil', userSessionCheck, usersController.profile);
+/* PUT - Edita datos de usuario */
+router.put('/perfil', profileValidator ,usersController.profileUpdate)
+/* POST - Creación de dirección */
+router.post('/direcciones', usersController.addressCreate);
+/* DELETE - Eliminación de dirección */
+router.delete('/direcciones/:id', usersController.addressDestroy);
 /*GET -Logout*/
 router.get('/logout', usersController.logout);
 
