59545230f6dc5c2741e7bd44a57fc974ea9958c7
Igor Villar <Theigor44@gmail.com>
2022-06-09 01:29:54 -0300

    Agrego categorias crud

--- src/controllers/admin/adminCategoriesController.js
+++ src/controllers/admin/adminCategoriesController.js
@@ -1,81 +1,89 @@
-const { getCategories, writeCategories} = require('../../data');
-
+const db = require("../../database/models")
 module.exports = {
     /* Envia la vista de listado de las categorias */
     list: (req, res) => {
-      res.render('admin/pages/categorias/listadoCategorias', {
-          titulo: "Categorías",
-          categorias:  getCategories,
-          session: req.session
-      })
+      db.Category.findAll()
+        .then((categorias) => {
+          res.render('admin/pages/categorias/listadoCategorias', {
+            titulo: "Categorías",
+            categorias,
+            session: req.session            
+          });
+        })
+        .catch((error) => res.send(error));
     },
     /* Envia la vista de formulario de creación de categorias */
     categoryAdd: (req, res) => {
-      res.render('admin/pages/categorias/agregarCategorias', 
+      res.render('admin/pages/categorias/agregarCategorias' , 
       { titulo: "Agregar categoría",
        session: req.session
       })
     },
     /* Recibe los datos del form de creación y guarda el categorias en la DB */
     categoryCreate: (req, res) => {
-      let lastId = 0;
-      getCategories.forEach(categoria => {
-        if(categoria.id > lastId){
-          lastId = categoria.id
-        }
-      });
-      
-      let newCategory = {
+      db.Category.create({
         name: req.body.name,
-        id: lastId + 1
-      };
-
-      getCategories.push(newCategory);
-
-      writeCategories(getCategories);
-
-      res.redirect('/admin/categorias');
+      })
+        .then((result) => {
+          res.redirect("/admin/categorias");
+        })
+        .catch((error) => res.send(error));
     },
     /* Envia la vista de form de edición de categorias */
     categoryEdit: (req, res) => {
       let categoryId = +req.params.id;
 
-      let categoria = getCategories.find(categoria => categoria.id === categoryId)
+      db.Category.findByPk(categoryId)
+      .then((categoria) => {
+        res.render("admin/pages/categorias/editarCategorias", {
+          titulo: "Editar categoria",
+          categoria,
+          session: req.session
 
-      res.render('admin/pages/categorias/editarCategorias', {
-        titulo: "Editar categoria",
-        categoria,
-        session: req.session
+        });
       })
+      .catch((error) => res.send(error));
     },
     /* Recibe los datos actualizados del form de edición */
     categoryUpdate: (req, res) => {
       let categoryId = +req.params.id;
       
-      getCategories.forEach(categoria => {
-        if(categoria.id === categoryId){
-          categoria.name = req.body.name
+      db.Category.update(
+        {
+          name: req.body.name,
+        },
+        {
+          where: {
+            id: categoryId,
+          },
         }
-      });
-
-      writeCategories(getCategories);
-
-      res.redirect('/admin/categorias');
+      )
+        .then((result) => {
+          if (result) {
+            res.redirect("/admin/categorias");
+          } else {
+            res.send("Ups ocurrio un error");
+          }
+        })
+        .catch((error) => res.send(error));
     },
     /* Recibe la info de la categoria a eliminar */
     categoryDelete: (req, res) => {
         let categoryId = +req.params.id;
 
-        getCategories.forEach(categoria => {
-            if(categoria.id === categoryId){
-                let categoryToDeleteIndex = getCategories.indexOf(categoria);
-                getCategories.splice(categoryToDeleteIndex, 1)
-            }
+        db.Category.destroy({
+          where: {
+            id: categoryId,
+          },
         })
-       
-        writeCategories(getCategories);
-       
-        res.redirect('/admin/categorias')
+          .then((result) => {
+            if (result) {
+              res.redirect("/admin/categorias");
+            } else {
+              res.send("Ups algo rompí");
+            }
+          })
+          .catch((error) => res.send(error));
     },
     /* Recibe los datos de la categoria a buscar */
     categorySearch: (req, res) => {
