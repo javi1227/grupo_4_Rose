016c38026e6aa9df750c4d0cb9d8cf9d4a504106
TobiasRoman <tobiasroman51@gmail.com>
2022-07-12 19:47:34 -0300

    asd

---
+++ public/js/productValidatorFront.js
@@ -0,0 +1,121 @@
+alert('dasd')
+
+
+function qs(element) {
+    return document.querySelector(element)
+}
+alert('dasd')
+window.addEventListener("load", () =>{
+    let $inputName = qs('#name'),
+    $inputPrice= qs('#price'),
+    $inputDiscount = qs('#discount'),
+    $inputCategory = qs('#category'),
+    $inputFile = qs('#image'),
+    $inputStock = qs('#stock'),
+    $inputDescription = qs('#description'),
+
+    $nameErrors = qs('#nameErrors'),
+    $priceErrors = qs('#priceErrors'),
+    $discountErrors = qs('#DiscountErrors'),
+    $categoryErrors = qs('#categoryErrors'),
+    $fileErrors = qs('#fileErrors'),
+    $stockErrors = qs('#stockErrors'),
+    $descriptionErrors = qs('#descriptionErrors'),
+
+
+    $imgPreview = qs('#img-preview'),
+    regExAlpha = /^[a-zA-Z\sñáéíóúü ]*$/,
+    regExDNI = /^[0-9]{7,8}$/,
+    regExEmail = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
+    regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/;
+
+
+    /* name validators */
+    $inputName.addEventListener("blur", (e) => {
+        let inputLength = $inputName.value.length;
+        switch (true) {
+            case !$inputName.value.trim(): /* trim elimina los espacios en blanco */
+                $nameErrors.innerHTML = "Campo requerido";
+                $inputName.classList.add("is-invalid");
+                break;
+            case inputLength <= 4:
+                $nameErrors.innerHTML = "Ingrese más de 4 caracteres"
+                $inputName.classList.add("is-invalid")
+                break;
+            case !regExAlpha.test($inputName.value):
+                $nameErrors.innerHTML = "Nombre inválido";
+                $inputName.classList.add("is-invalid");
+                break;
+            default:
+                
+                $inputName.classList.remove("is-invalid");
+                $inputName.classList.add("is-valid");
+                $nameErrors.innerHTML = "";
+                break;    
+            }
+        })
+
+    $inputPrice.addEventListener("blur", (e) => {
+        let inputLength = $inputPrice.value.length;
+        switch (true) {
+            case !$inputPrice.value.trim(): /* trim elimina los espacios en blanco */
+                $priceErrors.innerHTML = "Campo requerido";
+                $inputPrice.classList.add("is-invalid");
+                break;
+            case inputLength < 2:
+                $priceErrors.innerHTML = "Ingrese 2 números o más"
+                $inputPrice.classList.add("is-invalid")
+                break;
+            case !regExAlpha.test($inputPrice.value):
+                $priceErrors.innerHTML = "Nombre inválido";
+                $inputPrice.classList.add("is-invalid");
+                break;
+            default:
+                
+                $inputPrice.classList.remove("is-invalid");
+                $inputPrice.classList.add("is-valid");
+                $priceErrors.innerHTML = "";
+                break;    
+            }
+        })
+
+        $inputCategory.addEventListener('blur', ()=>{
+            if(!$inputCategory.value.trim()){
+                $categoryErrors.innerHTML = "Campo requerido"
+                $inputCategory.classList.add('is-invalid');
+            } else {
+                $inputCategory.classList.remove('is-invalid');
+                $inputCategory.classList.add('is-valid');
+                $categoryErrors.innerHTML = '';
+            }
+        })
+
+
+
+    $inputDescription.addEventListener("blur", (e) => {
+        let inputLength = $inputDescription.value.length;
+        switch (true) {
+            case !$inputDescription.value.trim(): /* trim elimina los espacios en blanco */
+                $descriptionErrors.innerHTML = "Campo requerido";
+                $inputDescription.classList.add("is-invalid");
+                break;
+            case inputLength < 2:
+                $descriptionErrors.innerHTML = "Ingrese 2 números o más"
+                $inputDescription.classList.add("is-invalid")
+                break;
+            case !regExAlpha.test($inputDescription.value):
+                $descriptionErrors.innerHTML = "Nombre inválido";
+                $inputDescription.classList.add("is-invalid");
+                break;
+            default:
+                
+                $inputDescription.classList.remove("is-invalid");
+                $inputDescription.classList.add("is-valid");
+                $descriptionErrors.innerHTML = "";
+                break;    
+            }
+        })
+
+
+
+})
--- src/controllers/admin/adminProductsController.js
+++ src/controllers/admin/adminProductsController.js
@@ -90,9 +90,10 @@ module.exports = {
           .catch((error) => res.send(error));
       },
     productUpdate: (req, res) => {
-        let idProducto = +req.params.id;
-        db.Product.update(
-            {
+      let errors = validationResult(req);
+
+      if(errors.isEmpty()){
+        db.Product.update({
               name: req.body.name,
               price: req.body.price,
               discount: req.body.discount,
@@ -102,18 +103,76 @@ module.exports = {
             },
             {
               where: {
-                id: idProducto,
+                id: req.params.id,
               },
+        })
+        .then(() => {
+          if(req.files !== undefined){
+            //1 - Preguntar si está subiendo imagenes
+            if(req.files.length > 0){
+              //2 - a. obtener todas las imágenes del proyecto
+              db.ProductImage.findAll({
+                where: {
+                  product_id: req.params.id,
+                }
+              })
+              .then((images) => {
+                //2 - b. hacer un array con los nombres de las imagenes.
+                let imageNames = images.map(image => image.imageName);
+                //3 - Eliminar imagenes del servidor
+                imageNames.forEach(image => {
+                  if(fs.existsSync(path.join(__dirname, `../../../public/images/products/${image}`))){
+                    fs.unlinkSync(path.join(__dirname, `../../../public/images/products/${image}`))
+                  }else{
+                    console.log("-- No se encontró el archivo");
+                  }
+                });
+                //4 - Eliminar las imágenes de la tabla
+                db.ProductImage.destroy({
+                  where: {
+                    product_id: req.params.id,
+                  }
+                })
+                .then(() => {
+                  //5 - Cargar nuevas imágenes
+                  let arrayImages = req.files.map(image => {
+                    return {
+                      imageName: image.filename,
+                      product_id: req.params.id
+                    } 
+                   })
+       
+                   db.ProductImage.bulkCreate(arrayImages)
+                   .then(() => res.redirect('/admin/productos'))
+                   .catch(error => console.log(error))
+                })
+                .catch(error => console.log(error))
+              })
+              .catch(error => console.log(error))
+            }else{
+              res.redirect('/admin/productos')
             }
-          )
-            .then((result) => {
-              if (result) {
-                res.redirect("/admin/productos");
-              } else {
-                res.send("Ups ocurrio un error");
-              }
-            })
-            .catch((error) => res.send(error));
+          }
+        })
+        .catch(error => console.log(error))
+      }else{
+        let idProducto = +req.params.id;
+        let productPromise = db.Product.findByPk(idProducto);
+        let categoriesPromise = db.Category.findAll();
+
+        Promise.all([productPromise, categoriesPromise])
+        .then(([producto, categories]) => {
+          res.render('admin/pages/productos/editarProducto', {
+            titulo: "Edición",
+            producto,
+            categories,
+            errors: errors.mapped(),
+            old: req.body,
+            session: req.session
+          })
+        })
+        .catch(error => console.log(error))
+      }
         },
     
     productDelete: (req, res) => {     
