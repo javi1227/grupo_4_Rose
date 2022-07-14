59fe5556d6a7817bbe7bc20ed693ddae06536a59
TobiasRoman <tobiasroman51@gmail.com>
2022-07-14 01:14:32 -0300

    se corrigieron validaciones

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -30,7 +30,7 @@ router.post('/productos', uploadProduct.array('imagen'), productsValidator, admi
 /* GET - Editar producto */
 router.get('/productos/editar/:id', userSessionCheck, adminCheck, adminProductsController.productEdit);
 /* PUT - Actualiza producto en la DB */
-router.put('/productos/:id', adminProductsController.productUpdate);
+router.put('/productos/:id', uploadProduct.array('imagen'), adminProductsController.productUpdate);
 /* DELETE - Elimina un producto */
 router.delete('/productos/eliminar/:id', adminProductsController.productDelete);
 
@@ -38,31 +38,28 @@ router.delete('/productos/eliminar/:id', adminProductsController.productDelete);
 
 
 router.get('/categorias',userSessionCheck, adminCheck, adminCategoriesController.list);
-/* GET - Agregar emprendimiento */
+
 router.get('/categorias/agregar',userSessionCheck, adminCheck, adminCategoriesController.categoryAdd);
-/* POST - Crea un emprendimiento en la DB */
+
 router.post('/categorias', adminCategoriesController.categoryCreate);
-/* GET - Editar emprendimiento */
+
 router.get('/categorias/editar/:id',userSessionCheck, adminCheck, adminCategoriesController.categoryEdit);
-/* PUT - Actualiza emprendimiento en la DB */
+
 router.put('/categorias/:id', adminCategoriesController.categoryUpdate);
-/* DELETE - Elimina un emprendimiento */
+
 router.delete('/categorias/eliminar/:id', adminCategoriesController.categoryDelete);
 module.exports = router;
 
 
-/* ============== */
-/* CRUD PRODUCTOS */
-/* ============== */
-/* GET - Lista de productos */
+
 router.get('/usuarios', userSessionCheck,adminUsersController.list);
-/* GET - Agregar producto */
+
 router.get('/usuarios/agregar', userSessionCheck,adminUsersController.usersAdd);
-/* POST - Crea un usero en la DB */
+
 router.post('/usuarios', uploadFile.any(''), adminUsersController.usersCreate);
-/* GET - Editar usero */
+
 router.get('/usuarios/editar/:id', userSessionCheck,  adminUsersController.usersEdit);
-/* PUT - Actualiza usero en la DB */
+
 router.put('/usuarios/:id', adminUsersController.usersUpdate);
-/* DELETE - Elimina un usero */
+
 router.delete('/usuarios/eliminar/:id', adminUsersController.usersDelete);
